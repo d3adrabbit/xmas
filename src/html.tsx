1ce15ad8e263c6540774a9f218ac467333c96379
d3adrabbit <49115041+d3adrabbit@users.noreply.github.com>
2022-12-21 11:37:56 +0800

    1 file modified

--- src/html.tsx
+++ src/html.tsx
@@ -17,6 +17,7 @@ function Html() {
     } else {
       musicRef.current?.play();
       if (active == false) {
+        frame();
         setActive(true);
       }
     }
@@ -27,36 +28,34 @@ function Html() {
     return Math.random() * (max - min) + min;
   };
 
-  // useLayoutEffect(() => {
-  //   (function frame() {
-  //     const duration = 15 * 1000;
-  //     const animationEnd = Date.now() + duration;
-  //     let skew = 1;
-  //     let timeLeft = animationEnd - Date.now();
-  //     let ticks = Math.max(200, 500 * (timeLeft / duration));
-  //     skew = Math.max(0.8, skew - 0.001);
+  const frame = () => {
+    const duration = 15 * 1000;
+    const animationEnd = Date.now() + duration;
+    let skew = 1;
+    let timeLeft = animationEnd - Date.now();
+    let ticks = Math.max(200, 500 * (timeLeft / duration));
+    skew = Math.max(0.8, skew - 0.001);
 
-  //     confetti({
-  //       particleCount: 1,
-  //       startVelocity: 0,
-  //       ticks: ticks,
-  //       origin: {
-  //         x: Math.random(),
-  //         // since particles fall down, skew start toward the top
-  //         y: Math.random() * skew - 0.2,
-  //       },
-  //       colors: ['#ffffff'],
-  //       shapes: ['circle'],
-  //       gravity: randomInRange(0.4, 0.6),
-  //       scalar: randomInRange(0.4, 1),
-  //       drift: randomInRange(-0.4, 0.4),
-  //     });
+    confetti({
+      particleCount: 1,
+      startVelocity: 0,
+      ticks: ticks,
+      origin: {
+        x: Math.random(),
+        // since particles fall down, skew start toward the top
+        y: Math.random() * skew - 0.2,
+      },
+      colors: ['#ffffff'],
+      shapes: ['circle'],
+      gravity: randomInRange(0.4, 0.6),
+      scalar: randomInRange(0.4, 1),
+      drift: randomInRange(-0.4, 0.4),
+    });
 
-  //     if (timeLeft > 0) {
-  //       requestAnimationFrame(frame);
-  //     }
-  //   })();
-  // }, []);
+    if (timeLeft > 0) {
+      requestAnimationFrame(frame);
+    }
+  };
 
   return (
     <>
